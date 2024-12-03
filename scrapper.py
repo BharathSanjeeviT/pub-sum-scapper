f9112ebb9ff99512cbe15cf055c9f9352136c021
bharathsanjeevit <bharathsanjeevi.t@gmail.com>
2024-12-03 22:46:04 +0530

    [FIX]: Refactor code structure

--- scrapper.py
+++ scrapper.py
@@ -1,79 +1,70 @@
 from flask import jsonify 
-from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+from browser import Playwright
+from utils import GS_URL, PARSER, CATEGORIES
+from html_scrapper import get_author_details, get_coauths, get_auth_pubs, get_pub_details, get_year_cits
 
 def fetch_author_gs_html(author_id: str) -> str:
-    url = f'https://scholar.google.com/citations?user={author_id}'
-    user_agent = (
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-        'AppleWebKit/537.36 (KHTML, like Gecko) '
-        'Chrome/91.0.4472.124 Safari/537.36'
-    )
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        context = browser.new_context()
-        page = context.new_page()
+    browser = Playwright()
+    page = browser.create_page_with_context()
 
-        page.set_extra_http_headers({'User-Agent': user_agent})
-        page.goto(url)
+    author_url = f'{GS_URL}/citations?user={author_id}'
+    page.goto(author_url)
 
-        while True:
-            try:
-                load_more_button = page.locator('#gsc_bpf_more')
-                if load_more_button.is_enabled():
-                    load_more_button.click()
-                    page.wait_for_timeout(1000)
-                else:
-                    break
-            except Exception:
-                break
+    load_more_button = page.locator('#gsc_bpf_more')
+    while load_more_button.is_enabled():
+        load_more_button.click()
+        # page.wait_for_timeout(1000)
 
-        content = page.content()
+    content = page.content()
+    browser.close_browser()
+    return content
 
-        browser.close()
-        return content
+def fetch_author_pub(author_pub_url):
+    browser = Playwright()
+    page = browser.create_page_with_context()
 
+    pub_html_content = []
+    for pub_url in author_pub_url:
+        page.goto(f'{GS_URL}/{pub_url}')
+        pub_html_content.append(page.content())
+
+    browser.close_browser()
+    return pub_html_content
+
+# main function 
 def get_author_gs(author_id: str):
     try:
         html_content = fetch_author_gs_html(author_id)
-        soup = BeautifulSoup(html_content, 'html.parser')
+        soup = BeautifulSoup(html_content, PARSER)
+
         author_pub = dict()
+        get_author_details(soup, author_pub)
+        author_pub['co_authors'] = get_coauths(soup)
+        author_pub['year-cits'] = get_year_cits(soup)
+        author_pub_url = get_auth_pubs(soup)
 
-        auth_name_html = soup.find(id='gsc_prf_in')
-        if auth_name_html:
-            author_pub['name'] = auth_name_html.get_text()
+        pub_html_content = fetch_author_pub(author_pub_url)
+        publications = get_publications(pub_html_content)
 
-        author_deatils_html = soup.find(id='gsc_rsb_st').find('tbody').find_all('tr')
-        author_pub['cits'] = author_deatils_html[0].find(class_='gsc_rsb_std').get_text()
-        author_pub['h'] = author_deatils_html[1].find(class_='gsc_rsb_std').get_text()
-
-        co_auths = []
-        co_auths_list = soup.find(class_='gsc_rsb_a')
-        for co_auth in co_auths_list.find_all('li'):
-            co_auths.append(co_auth.find('a').get_text())
-        author_pub['co_authors'] = co_auths
-
-        publications = []
-        pub_table_html = soup.find(id='gsc_a_t')
-        if pub_table_html:
-            pub_tbody_html = pub_table_html.find('tbody')
-            if pub_tbody_html:
-                for trow in pub_tbody_html.find_all('tr'):
-                    pub_details = dict()
-                    pub_details = dict()
-                    tdata = trow.find_all('td')
-                    pub_details['title'] = tdata[0].find('a').get_text()
-                    more_info = tdata[0].find_all('div')
-                    pub_details['co_auhtors'] = more_info[0].get_text()
-                    pub_details['desc'] = more_info[1].get_text()
-                    pub_details['cits'] = tdata[1].get_text()
-                    pub_details['year'] = tdata[2].get_text()
-                    publications.append(pub_details)
-                author_pub["pubs"] = publications
         return jsonify({
             'status': 200, 
-            'author_pub': author_pub
+            **author_pub,
+            'publications': publications
         })
+
     except Exception as err:
         return jsonify({ 'status': 400, 'err': err })
+
+def get_publications(pub_html_content):
+    publications = dict()
+    for pub in pub_html_content:
+        pub_soup = BeautifulSoup(pub, PARSER)
+        pub_obj = get_pub_details(pub_soup)
+        for category, keys in CATEGORIES.items():
+            if all(key in pub_obj for key in keys):  
+                if category not in publications:
+                    publications[category] = []
+                publications[category].append(pub_obj)
+    return publications
 
---
+++ utils.py
@@ -0,0 +1,11 @@
+GS_URL = 'https://scholar.google.com'
+PARSER = 'html.parser'
+CATEGORIES = {
+    "journal": ["Journal", "Volume", "Issue"],
+    "conference": ["Conference", "Proceedings", "Symposium"],
+    "book": ["Book", "Publisher", "Isbn"],
+    "preprint": ["Preprint", "Arxiv", "Biorxiv", "Ssrn"],
+    "thesis": ["Thesis", "Dissertation", "University"],
+    "patent": ["Patent"],
+    "report": ["Report", "Technical Report"]
+}
